87d335ba1b11b76bfccad8ff4b488a89cd229499
Yulia Shlykova <jshlykova@spbtv.com>
2022-02-15 08:35:54 +0300

    Completed Guess the flag demo app tasks.

--- GuessFlag/GuessFlag/ContentView.swift
+++ GuessFlag/GuessFlag/ContentView.swift
@@ -10,46 +10,76 @@ import SwiftUI
 struct ContentView: View {
     @State private var scoreTitle = ""
     @State private var showScore = false
+    @State private var score: Int = 0
     
     @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)],
+                           center: .top,
+                           startRadius: 200.0,
+                           endRadius: 700.0)
                 .ignoresSafeArea()
-            VStack(spacing: 30.0) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundColor(.white)
-                        .font(.subheadline.weight(.heavy))
-                    
-                    Text("\(countries[correctAnswer])")
-                        .foregroundColor(.white)
-                        .font(.largeTitle.weight(.semibold))
-                }
+            
+            VStack {
+                Spacer()
                 
-                ForEach ((0..<3), content: { number in
-                    Button {
-                        flagButtonTapped(number)
-                    } label: {
-                        Image(countries[number])
-                            .renderingMode(.original)
-                            .cornerRadius(12.0)
-                            .shadow(radius: 10.0)
+                Text("Guess the Flag")
+                    .foregroundColor(.white)
+                    .font(.largeTitle.weight(.bold))
+                
+                VStack(spacing: 15.0) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text("\(countries[correctAnswer])")
+                            .foregroundStyle(.secondary)
+                            .font(.largeTitle.weight(.semibold))
                     }
-                })
+                    
+                    ForEach ((0..<3), content: { number in
+                        Button {
+                            flagButtonTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .cornerRadius(12.0)
+                                .shadow(radius: 10.0)
+                        }
+                    })
+                }
+                .frame(maxWidth: .infinity)
+                .padding(20.0)
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20.0))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                Spacer()
             }
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showScore) {
             Button("Continue", action: generateQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(score)")
         }
     }
     
     func flagButtonTapped(_ number: Int) {
-        scoreTitle = (number == correctAnswer) ? "Correct" : "Wrong"
+        scoreTitle = (number == correctAnswer) ? "Correct" : "Wrong! That's the flag of \(countries[number])"
+        score += (number == correctAnswer) ? 1 : 0
         showScore = true
     }
     
